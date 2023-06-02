5773891ae1b82567220bcd385181cb829a2cb136
Dani Rodríguez <dani@danirod.es>
2023-06-02 19:43:15 +0200

    postgres: lint SQL file

--- postgresql/escuela/escuela.sql
+++ postgresql/escuela/escuela.sql
@@ -17,14 +17,14 @@ CREATE TABLE IF NOT EXISTS profesores (
 CREATE TABLE IF NOT EXISTS asignaturas (
     id bigserial NOT NULL PRIMARY KEY,
     nombre character varying(64),
-    profesor bigint REFERENCES profesores(id)
+    profesor bigint REFERENCES profesores (id)
 );
 
 CREATE TABLE IF NOT EXISTS matriculas
 (
     id bigserial NOT NULL PRIMARY KEY,
-    alumno bigint REFERENCES alumnos(id),
-    asignatura bigint REFERENCES asignaturas(id),
+    alumno bigint REFERENCES alumnos (id),
+    asignatura bigint REFERENCES asignaturas (id),
     curso bigint,
     calificacion smallint
 );
