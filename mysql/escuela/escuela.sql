ac35ad9e22e1258d2ea9aced8a5d4e9dec6869c6
Dani Rodríguez <dani@danirod.es>
2023-06-02 19:42:57 +0200

    mysql: unique not needed if already primary key

--- mysql/escuela/escuela.sql
+++ mysql/escuela/escuela.sql
@@ -2,22 +2,19 @@ CREATE TABLE alumnos (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(32) NOT NULL,
     apellidos VARCHAR(64) NOT NULL,
-    fecha_nacimiento DATE NOT NULL,
-    UNIQUE KEY alumnos_id (id)
+    fecha_nacimiento DATE NOT NULL
 );
 
 CREATE TABLE profesores (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(32) NOT NULL,
-    apellidos VARCHAR(64) NOT NULL,
-    UNIQUE KEY profesores_id (id)
+    apellidos VARCHAR(64) NOT NULL
 );
 
 CREATE TABLE asignaturas (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(64) NOT NULL,
     profesor INT NOT NULL,
-    UNIQUE KEY asignaturas_id (id),
     FOREIGN KEY asignaturas_profesores (profesor) REFERENCES profesores (id)
 );
 
@@ -27,7 +24,6 @@ CREATE TABLE matriculas (
     asignatura INT NOT NULL,
     fecha YEAR NOT NULL,
     nota INT,
-    UNIQUE KEY matriculas_id (id),
     FOREIGN KEY matriculas_alumnos (alumno) REFERENCES alumnos (id),
     FOREIGN KEY matriculas_asignaturas (asignatura) REFERENCES asignaturas (id)
 );
